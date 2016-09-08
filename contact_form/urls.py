b53157daedcd61527936685528b751a4c507432d
Ravlenze <ravelnze@icloud.com>
2016-09-08 19:46:12 +0800

    minor changes

--- contact_form/urls.py
+++ contact_form/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^', views.load_form, name='form'),
     url(r'^thanks/', views.complete_form, name='complete'),
+    url(r'^', views.load_form, name='form'),
 ]
--- contact_form/views.py
+++ contact_form/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from forms import EnquiryForm
+from django.core.urlresolvers import reverse
 
 
 def load_form(request):
     if request.method == 'POST':
         form = EnquiryForm(request.POST)
         if form.is_valid():
+            # Need to add email functionality.
             form.save()
-            return redirect('contact:complete')
+            return redirect(reverse('contact:complete'))
     else:
         form = EnquiryForm()
 
     context = {'form': form}
-
     return render(request, 'contact.html', context)
 
 
