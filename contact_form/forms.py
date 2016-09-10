54d4ae51b6ee78ec61e818be8710ca65bf97d15f
Ravlenze <ravelnze@icloud.com>
2016-09-10 14:29:05 +0800

    form helper implementation

--- contact_form/forms.py
+++ contact_form/forms.py
@@ -1,8 +1,49 @@
 from django import forms
 from models import Enquiry
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, Fieldset, ButtonHolder, HTML
+
+
+class SubmitDefault(Submit):
+    field_classes = 'btn'
 
 
 class EnquiryForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(EnquiryForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].label = 'First'
+        self.fields['last_name'].label = 'Last'
+        self.fields['email_contact'].label = 'Email Address'
+        self.fields['phone_contact'].label = 'Phone Number'
+        self.fields['wedding_date'].label = 'Wedding Date'
+        self.fields['comments'].label = 'Comments/Questions'
+        self.helper = FormHelper()
+        self.helper.form_id = 'id_contact_form'
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'lower-pad'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Send John an enquiry for an obligation free quote:',
+                HTML(
+                    '<label class="control-label">Name:</label>'
+                ),
+                Div(
+                    'first_name',
+                    'last_name',
+                    css_class='form-inline'
+                ),
+                'email_contact',
+                'phone_contact',
+                'venue',
+                'wedding_date',
+                'comments'
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit', css_class='btn btn-default')
+            )
+        )
+
     class Meta:
         model = Enquiry
         fields = [
