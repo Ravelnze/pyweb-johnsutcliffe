437af47e254ea6d8154e1e7497c2807cc42a5985
Ravlenze <ravelnze@icloud.com>
2016-09-10 18:20:47 +0800

    added datepicker to date field

--- contact_form/admin.py
+++ contact_form/admin.py
@@ -4,4 +4,4 @@ from models import Enquiry
 
 @admin.register(Enquiry)
 class EnquiryAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email_contact']
+    list_display = ['first_name', 'last_name', 'email_contact', 'wedding_date', 'venue']
--- contact_form/forms.py
+++ contact_form/forms.py
@@ -9,32 +9,51 @@ class EnquiryForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(EnquiryForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].label = 'First'
-        self.fields['last_name'].label = 'Last'
-        self.fields['email_contact'].label = 'Email Address'
-        self.fields['phone_contact'].label = 'Phone Number'
-        self.fields['wedding_date'].label = 'Wedding Date'
-        self.fields['comments'].label = 'Comments/Questions'
+        self.fields['first_name'].label = ''
+        self.fields['last_name'].label = ''
+        self.fields['email_contact'].label = ''
+        self.fields['phone_contact'].label = ''
+        self.fields['venue'].label = ''
+        self.fields['wedding_date'].label = ''
+        self.fields['comments'].label = ''
         self.helper = FormHelper()
         self.helper.form_id = 'id_contact_form'
         self.helper.form_method = 'POST'
-        self.helper.form_class = 'lower-pad'
         self.helper.layout = Layout(
             Fieldset(
                 'Send John an enquiry for an obligation free quote:',
                 HTML(
-                    '<label class="control-label">Name:</label>',
+                    '<label class="control-label">Name</label>',
                 ),
                 Div(
-                    'first_name',
-                    'last_name',
+                    Field('first_name', placeholder='First Name'),
+                    Field('last_name', placeholder='Last Name'),
                     css_class='form-inline'
                 ),
-                'email_contact',
-                Field('phone_contact', placeholder='0412345678'),
-                Field('venue', placeholder='Venue Name'),
-                Field('wedding_date', placeholder='01/01/2000'),
-                Field('comments', placeholder='Is there anything else you would like to know?')
+                HTML(
+                    '<br />'
+                    '<label class="control-label">Contact Details</label>',
+                ),
+                Div(
+                    Field('email_contact', placeholder='Email Address'),
+                    Field('phone_contact', placeholder='Phone Number'),
+                    css_class='form-inline'
+                ),
+                HTML(
+                    '<br />'
+                    '<label class="control-label">Wedding Details</label>',
+                ),
+                Div(
+                    Field('venue', placeholder='Venue Name'),
+                    Field('wedding_date', placeholder='Wedding Date'),
+                    # Field('wedding_date', template='datepicker.html'),
+                    css_class='form-inline'
+                ),
+                HTML(
+                    '<br />'
+                    '<label class="control-label">Comments/Questions</label>',
+                ),
+                Field('comments', placeholder='Anything else you would like to know?')
             ),
             ButtonHolder(
                 StrictButton('Submit', css_class='btn-default', type='submit')
