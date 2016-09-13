ee68b31abb3214214b8117c3555e0aef1ac58d2f
Ravlenze <ravelnze@icloud.com>
2016-09-13 21:57:04 +0800

    added dynamic field sizes to contact form

--- contact_form/forms.py
+++ contact_form/forms.py
@@ -7,51 +7,49 @@ from johnsutcliffe.settings import DATE_INPUT_FORMATS
 
 
 class EnquiryForm(forms.ModelForm):
-    wedding_date = forms.DateField(input_formats=DATE_INPUT_FORMATS)
+    wedding_date = forms.DateField(input_formats=DATE_INPUT_FORMATS, required=False)
 
     def __init__(self, *args, **kwargs):
         super(EnquiryForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].label = ''
-        self.fields['last_name'].label = ''
-        self.fields['email_contact'].label = ''
-        self.fields['phone_contact'].label = ''
-        self.fields['venue'].label = ''
-        self.fields['wedding_date'].label = ''
-        self.fields['comments'].label = ''
+        # Helper definition
         self.helper = FormHelper()
         self.helper.form_id = 'id_contact_form'
         self.helper.form_method = 'POST'
+        self.helper.form_show_labels = False
+        self.helper.form_class = 'lower-pad'
+        # self.helper.field_template = 'bootstrap/layout/inline_field.html'
         self.helper.layout = Layout(
             Fieldset(
-                'Send John an enquiry for an obligation free quote:',
+                'Send John an enquiry for an obligation free quote',
                 HTML(
                     '<label class="control-label">Name</label>',
                 ),
                 Div(
-                    Field('first_name', placeholder='First Name'),
-                    Field('last_name', placeholder='Last Name'),
-                    css_class='form-inline'
+                    Field('first_name', placeholder='First Name', template='dynamic_text_input.html'),
+                    Field('last_name', placeholder='Last Name', template='dynamic_text_input.html'),
+                    css_class='row'
                 ),
                 HTML(
-                    '<br />'
+                    # '<br />'
                     '<label class="control-label">Contact Details</label>',
                 ),
                 Div(
-                    Field('email_contact', placeholder='Email Address'),
-                    Field('phone_contact', placeholder='Phone Number'),
-                    css_class='form-inline'
+                    Field('email_contact', placeholder='Email Address', template='dynamic_text_input.html'),
+                    Field('phone_contact', placeholder='Phone Number', template='dynamic_text_input.html'),
+                    css_class='row'
                 ),
                 HTML(
-                    '<br />'
+                    # '<br />'
                     '<label class="control-label">Wedding Details</label>',
                 ),
                 Div(
-                    Field('venue', placeholder='Venue Name'),
-                    Field('wedding_date', placeholder='Wedding Date', readonly="True"),
-                    css_class='form-inline'
+                    Field('venue', placeholder='Venue Name', template='dynamic_text_input.html'),
+                    Field('wedding_date', placeholder='Wedding Date', readonly="True",
+                          template='dynamic_text_input.html'),
+                    css_class='row'
                 ),
                 HTML(
-                    '<br />'
+                    # '<br />'
                     '<label class="control-label">Comments/Questions</label>',
                 ),
                 Field('comments', placeholder='Anything else you would like to know?')
